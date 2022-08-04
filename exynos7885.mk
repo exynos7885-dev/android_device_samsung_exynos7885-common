9878b071853c3bb5e8ecb04b25edf05559e61bac
A2L5E0X1 <alexed25@protonmail.com>
2022-08-04 19:34:32 +0200

    exynos7885-common: import overlays
    * based on exynos9820-common overlays (branch: lineage-18.1), reworked for 7885.
    
    Change-Id: I42f04d62e7e82b9f5e1a0f8c454fe8e6981e69e4

--- exynos7885.mk
+++ exynos7885.mk
@@ -137,6 +137,10 @@ PRODUCT_PACKAGES += \
     NfcNci \
     Tag
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS := $(COMMON_PATH)/overlay
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
