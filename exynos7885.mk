f0119821a248aa8f3d699fbe2bd8f7127327f00c
A2L5E0X1 <alexed25@protonmail.com>
2022-10-09 01:20:11 +0200

    exynos7885-common: build libkeymaster4support
    Change-Id: Ibabf450eced92ace7b828a20b9dea53326d044d1

--- exynos7885.mk
+++ exynos7885.mk
@@ -120,7 +120,8 @@ PRODUCT_COPY_FILES += \
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service
+    android.hardware.keymaster@3.0-service \
+    libkeymaster4support
 
 # Lights
 PRODUCT_PACKAGES += \
